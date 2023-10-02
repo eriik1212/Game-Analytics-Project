624478ebb21697c38c59a525ffff25cc70c2f7b9
David Boces Obis <79161189+VoZeS@users.noreply.github.com>
2023-10-02 14:23:33 +0200

    DeleteUserScriptAndNewSessionDeadline

--- ProjectSim/Assets/_Scripts/SenderData.cs
+++ ProjectSim/Assets/_Scripts/SenderData.cs
@@ -1,26 +1,34 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Networking;
-using System.Collections;
 
 public class SenderData : MonoBehaviour
 {
-
     public string serverURL = "https://tu-servidor.com/api"; // Reemplaza con la URL de tu servidor
 
-    public void SendData(string name, string country, string date)
+    private void OnEnable()
     {
-        StartCoroutine(SendDataCoroutine(name, country, date));
+        Simulator.OnNewPlayer += SendData;   
+    }
+    private void OnDisable()
+    {
+        Simulator.OnNewPlayer -= SendData;   
     }
 
-    private IEnumerator SendDataCoroutine(string name, string country, string date)
+    public void SendData(string name, string country, DateTime date)
+    {
+        StartCoroutine(SendUserDataCoroutine(name, country, date));
+    }
+
+    private IEnumerator SendUserDataCoroutine(string name, string country, DateTime date)
     {
         // Crear un formulario para los datos
         WWWForm form = new WWWForm();
         form.AddField("Name", name);
         form.AddField("Country", country);
-        form.AddField("Date", date);
+        form.AddField("Date", date.ToString());
 
         // Crear una solicitud POST con el formulario
         UnityWebRequest www = UnityWebRequest.Post(serverURL, form);
@@ -31,10 +39,23 @@ public class SenderData : MonoBehaviour
         // Verificar si hubo un error en la solicitud
         if (www.result == UnityWebRequest.Result.Success)
         {
+            uint userId = 2; // !!!!! TEMPORAL - La ID aquesta ens la inventem
+
+            CallbackEvents.OnAddPlayerCallback?.Invoke(userId);
+
             Debug.Log("Datos enviados con éxito al servidor.");
         }
         else
         {
+            // ----------------------------------------------------------------------- TEMPORAL - Això ha de passar només quan es SUCCESS
+            //
+                uint userId = 2; // !!!!! TEMPORAL - La ID aquesta ens la inventem
+            //
+            //
+                CallbackEvents.OnAddPlayerCallback?.Invoke(userId);
+            //
+            // -----------------------------------------------------------
+
             Debug.LogError("Error al enviar datos al servidor: " + www.error);
         }
     }
