4c3a4126cd3d5138e668fa24e2eae68ac5af6eb6
David Boces Obis <79161189+VoZeS@users.noreply.github.com>
2023-10-16 13:41:44 +0200

    Update ServerPhP.php

--- ServerPhP.php
+++ ServerPhP.php
@@ -14,86 +14,56 @@ if (!$connection) {
 if ($_SERVER["REQUEST_METHOD"] === "POST") {
   // ------------------------------------------------------------------------------ USER
   // ---------------------------------------------------- Nombre
-    if (isset($_POST["Name"])) {
+    if (isset($_POST["Name"]) && isset($_POST["Age"]) && isset($_POST["Gender"]) && isset($_POST["Country"]) && isset($_POST["Date"])) {
+
       $name = $_POST["Name"];
-    } else {
-    // Manejo de la situacion en la que "name" no esta definido en $_POST
-      $name = "Nombre no definido";
-    }
-
-    // ---------------------------------------------------- Edad
-    if (isset($_POST["Age"])) {
       $age = $_POST["Age"];
-    } else {
-    // Manejo de la situacion en la que "name" no esta definido en $_POST
-      $age = "Edad no definida";
-    }
-
-    // ---------------------------------------------------- Genero
-    if (isset($_POST["Gender"])) {
       $gender = $_POST["Gender"];
-    } else {
-    // Manejo de la situacion en la que "name" no esta definido en $_POST
-      $gender = "Genero no definido";
-    }
-
-    // ---------------------------------------------------- Pais
-    if (isset($_POST["Country"])) {
       $country = $_POST["Country"];
-    } else {
-    // Manejo de la situacion en la que "name" no esta definido en $_POST
-      $country = "Country no definido";
-    }
-
-    // ---------------------------------------------------- FirstLoginDate
-    if (isset($_POST["Date"])) {
       $date = $_POST["Date"];
-    } else {
-    // Manejo de la situacion en la que "name" no esto definido en $_POST
-      $date = "First Login Date no definido";
-    }
 
-    // INSERT INTO
-    $sql = "INSERT INTO User(Username, Age, Gender, Country, FirstLoginDate) VALUES ('$name', '$age', '$gender', '$country', '$date')";
+      // INSERT INTO
+      $sql = "INSERT INTO User (Username, Age, Gender, Country, FirstLoginDate) VALUES ('$name', '$age', '$gender', '$country', '$date')";
 
-    if ($connection->query($sql) === TRUE) {
-      // Obtiene la última ID generada
-      $lastUserID = $connection->insert_id;
-  
-      // Imprime la última ID generada
-      echo $lastUserID;
-    } else {
-      // Manejar errores de inserción
-      echo "Error al crear el registro: " . $connection->error;
-    }
+      if ($connection->query($sql) === TRUE) {
+        // Obtiene la última ID generada
+        $lastUserID = $connection->insert_id;
     
+        // Imprime la última ID generada
+        echo $lastUserID;
+      } else {
+        // Manejar errores de inserción
+        echo "Error al crear el registro: " . $connection->error;
+      }
+
+    } 
 
     // ------------------------------------------------------------------------------
     // ------------------------------------------------------------------------------ SESSION
 
-    if (isset($_POST["startSessionTime"])) {
+    else if (isset($_POST["startSessionTime"]) && isset($_POST["endSessionTime"])) {
       $startSessionTime = $_POST["startSessionTime"];
-    } else {
-    // Manejo de la situacion en la que "time" no esto definido en $_POST
-      $startSessionTime = "Start Session Time no definido";
-    }
-
-    // INSERT INTO
-    $sql = "INSERT INTO `Sessions` (`Start`) VALUES ('$startSessionTime')";
-
-    // if ($connection->query($sql) === TRUE) {
-    //   // Obtiene la última ID generada
-    //   $lastSessionID = $connection->insert_id;
-  
-    //   // Imprime la última ID generada
-    //   //echo $lastSessionID;
-    // } else {
-    //   // Manejar errores de inserción
-    //   echo "Error al crear el registro: " . $connection->error;
-    // }
-    // ------------------------------------------------------------------------------
-
+      $endSessionTime = $_POST["endSessionTime"];
     
+      // INSERT INTO
+      $sql = "INSERT INTO `Sessions`(`Start`, `End`, user_id) VALUES ('$startSessionTime', '$endSessionTime' , 3)";
+
+    if ($connection->query($sql) === TRUE) {
+      // Obtiene la última ID generada
+      $lastSessionID = $connection->insert_id;
+  
+      // Imprime la última ID generada
+      //echo $lastSessionID;
+    } else {
+      // Manejar errores de inserción
+      echo "Error al crear el registro: " . $connection->error;
+    }
+    // ------------------------------------------------------------------------------
+  }
+  else 
+  {
+    echo "Form no valido";
+  }
     // CLOSE
     mysqli_close($connection);
 }
